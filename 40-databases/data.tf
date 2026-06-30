d1a6879059b072f05869f96468b92309953cf5ef
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-06-30 18:24:46 +0800

    update

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -5,8 +5,13 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
+
 data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project}/${var.environment}/redis_sg_id"
+}
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 
 
